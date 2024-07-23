de2b4ea47d0be6057bab65db93710f5b57fdeb24
Quentin D. <quentin.didelot13@gmail.com>
2024-07-23 09:32:57 +0200

    Étape 5 - Gestion des employés
    - Affichage de la page "employes-details" : done
    - Modification des données des employés : done
    - Suppression des employés : done

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
-use finfo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Employe;
+use App\Form\EmployeType;
+
 
 class EmployeController extends AbstractController
 {
@@ -31,5 +33,50 @@ class EmployeController extends AbstractController
         $employes = $this->employeRepository->findAll();
         
         return $this->render('employes-liste.html.twig', ['employes' => $employes]);
+    }
+
+    /**
+     * Afficher les détails d'un employé avec la possibilité de le modifier
+     */
+    #[Route('/employe/{id}', name: 'app_employe_detail')]
+    public function detailEmploye(int $id, Request $request): Response {
+        $employe = $this->employeRepository->find($id);
+
+        if (!$employe) {
+            throw $this->createNotFoundException('Aucun employé trouvé avec cet identifiant.');
+        }
+
+        // Formulaire pour modifier un employé
+        $form = $this->createForm(EmployeType::class, $employe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->entityManager->persist($employe);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_employe_detail', ['id' => $employe->getId()]);
+        }
+
+        return $this->render('employes-details.html.twig', [
+            'employe' => $employe,
+            'form' => $form->createView()]);
+    }
+
+    /**
+     * Supprimer un employé
+     */
+    #[Route('/employe/{id}/delete', name: 'app_employe_delete')] 
+    public function deleteEmploye(int $id): Response {
+        $employe = $this->employeRepository->find($id);
+
+        if (!$employe) {
+            throw $this->createNotFoundException('Aucun employé trouvé avec cet identifiant.');
+        }
+
+        $this->entityManager->remove($employe);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('app_employe_list');
     }
 }
---
+++ src/Form/EmployeType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employe;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmployeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+                'attr' => [
+                    'maxlength' => 255,
+                ],
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'required' => true,
+                'attr' => [
+                    'maxlength' => 255,
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true,
+            ])
+            ->add('contrat', TextType::class, [
+                'label' => 'Statut',
+                'required' => true,
+                'attr' => [
+                    'maxlength' => 255,
+                ],
+            ])
+            ->add('dateArrivee', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date d\'entrée',
+                'required' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Employe::class,
+        ]);
+    }
+}
