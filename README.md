b7f4c543802750b25c1ae6bb9e01b13d6d5575d5
Quentin D. <quentin.didelot13@gmail.com>
2024-08-15 16:32:55 +0200

    Modification des fixtures

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 TaskLinker est une plateforme permettant de gérer les projets de l'entreprise BeWise.
 
-Ce dépôt correspond à la correction de l'exercice du projet 10 du parcours PHP / Symfony. Chaque étape dispose d'une correction dans les branches correction-etape-X.
-
-Le dépôt contient également [la mise en place d'une seconde authentification](https://github.com/OpenClassrooms-Student-Center/876-p10-m1-correction/tree/correction-etape-5) dans le cadre de l'étape facultative. **Google Authenticator** a été mis en place pour cette correction.
-
 ## Installation
 
 1. Télécharger le projet
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,16 +10,12 @@ use App\Entity\Projet;
 use App\Entity\Tache;
 use App\Entity\Statut;
 use \DateTime;
-use \DateInterval;
 
 class AppFixtures extends Fixture
 {
-
     public function __construct(
         private UserPasswordHasherInterface $hasher
-    )
-    {
-
+    ) {
     }
 
     public function load(ObjectManager $manager): void
@@ -37,127 +33,110 @@ class AppFixtures extends Fixture
         $done->setLibelle('Done');
         $manager->persist($done);
 
-        
         // Création des employés
         $employe1 = new Employe();
-        $employe1->setNom('Dillon')
-            ->setPrenom('Natalie')
-            ->setEmail('natalie@driblet.com')
+        $employe1->setNom('Sheppard')
+            ->setPrenom('John')
+            ->setEmail('john@me.com')
             ->setStatut('CDI')
             ->setRoles(['ROLE_ADMIN'])
-            ->setPassword($this->hasher->hashPassword($employe1, '123456'))
-            ->setDateArrivee(new DateTime('2019-06-14'));
+            ->setPassword($this->hasher->hashPassword($employe1, 'john'))
+            ->setDateArrivee(new DateTime('2024-08-15'));
         $manager->persist($employe1);
 
         $employe2 = new Employe();
-        $employe2->setNom('Baker')
-            ->setPrenom('Demi')
-            ->setEmail('demi@driblet.com')
-            ->setStatut('CDD')
-            ->setPassword($this->hasher->hashPassword($employe2, 'd3mibak3r'))
-            ->setDateArrivee(new DateTime('2022-09-01'));
+        $employe2->setNom('Vakarian')
+            ->setPrenom('Garrus')
+            ->setEmail('garrus@me.com')
+            ->setStatut('CDI')
+            ->setRoles([])
+            ->setPassword($this->hasher->hashPassword($employe2, 'garrus'))
+            ->setDateArrivee(new DateTime('2024-08-15'));
         $manager->persist($employe2);
 
         $employe3 = new Employe();
-        $employe3->setNom('Dupont')
-            ->setPrenom('Marie')
-            ->setEmail('marie@driblet.com')
-            ->setPassword($this->hasher->hashPassword($employe3, '011216'))
-            ->setStatut('Freelance')
-            ->setDateArrivee(new DateTime('2021-12-20'));
+        $employe3->setNom('T\'Soni')
+            ->setPrenom('Liara')
+            ->setEmail('liara@me.com')
+            ->setStatut('CDI')
+            ->setRoles([])
+            ->setPassword($this->hasher->hashPassword($employe3, 'liara'))
+            ->setDateArrivee(new DateTime('2024-08-15'));
         $manager->persist($employe3);
+
+        $employe4 = new Employe();
+        $employe4->setNom('Alenko')
+            ->setPrenom('Kaidan')
+            ->setEmail('kaidan@me.com')
+            ->setStatut('CDD')
+            ->setRoles([])
+            ->setPassword($this->hasher->hashPassword($employe4, 'kaidan'))
+            ->setDateArrivee(new DateTime('2024-08-15'));
+        $manager->persist($employe4);
+
+        $employe5 = new Employe();
+        $employe5->setNom('Lawson')
+            ->setPrenom('Miranda')
+            ->setEmail('miranda@me.com')
+            ->setStatut('Freelance')
+            ->setRoles([])
+            ->setPassword($this->hasher->hashPassword($employe5, 'miranda'))
+            ->setDateArrivee(new DateTime('2024-08-15'));
+        $manager->persist($employe5);
 
         // Création des projets
         $projet1 = new Projet();
-        $projet1
-            ->setNom('TaskLinker')
-            ->setArchive(false)
-            ->addEmploye($employe1)
-            ->addEmploye($employe2);
+        $projet1->setNom('TaskLinker')
+            ->setArchive(true);
         $manager->persist($projet1);
 
         $projet2 = new Projet();
-        $projet2
-            ->setNom('Application mobile Grand Nancy')
-            ->setArchive(true)
-            ->addEmploye($employe2)
-            ->addEmploye($employe3);
+        $projet2->setNom('test')
+            ->setArchive(true);
         $manager->persist($projet2);
 
         $projet3 = new Projet();
-        $projet3
-            ->setNom('Site vitrine Les Soeurs Marchand')
-            ->setArchive(false)
-            ->addEmploye($employe1)
-            ->addEmploye($employe3);
+        $projet3->setNom('Citadelle')
+            ->setArchive(false);
+        $projet3->addEmploye($employe1)
+            ->addEmploye($employe2)
+            ->addEmploye($employe3)
+            ->addEmploye($employe4);
         $manager->persist($projet3);
 
-        // Création des tâches
-        $tache0 = new Tache();
-        $tache0->setTitre('Développement de la structure globale')
-            ->setDescription('Intégrer les maquettes')
-            ->setStatut($done)
-            ->setEmploye($employe2)
-            ->setProjet($projet1)
-            ->setDeadline((new DateTime())->sub(new DateInterval('P7D')));
-        $manager->persist($tache0);
+        $projet4 = new Projet();
+        $projet4->setNom('Cerberus')
+            ->setArchive(false);
+        $projet4->addEmploye($employe5);
+        $manager->persist($projet4);
 
+        // Création des tâches
         $tache1 = new Tache();
-        $tache1->setTitre('Développement de la page projet')
-            ->setDescription('Page projet avec liste des tâches, édition, modification, suppression et création des tâches')
-            ->setStatut($done)
-            ->setEmploye($employe1)
-            ->setProjet($projet1);
+        $tache1->setTitre('Test (To Do)')
+            ->setDescription('Une tâche à faire')
+            ->setStatut($todo)
+            ->setEmploye($employe2)
+            ->setProjet($projet3)
+            ->setDeadline(new DateTime('2024-08-17'));
         $manager->persist($tache1);
 
         $tache2 = new Tache();
-        $tache2->setTitre('Développement de la page employé')
-            ->setDescription('Page employé avec liste des employés, édition, modification, suppression et création des employés')
+        $tache2->setTitre('Test (Doing)')
+            ->setDescription('Une tâche en cours')
             ->setStatut($doing)
             ->setEmploye($employe2)
-            ->setDeadline((new DateTime())->add(new DateInterval('P4D')))
-            ->setProjet($projet1);
+            ->setProjet($projet3)
+            ->setDeadline(new DateTime('2024-08-16'));
         $manager->persist($tache2);
 
         $tache3 = new Tache();
-        $tache3->setTitre('Gestion des droits d\'accès')
-            ->setDescription('Un employé ne peut accéder qu\'à ses projets')
-            ->setStatut($todo)
-            ->setDeadline((new DateTime())->add(new DateInterval('P12D')))
-            ->setProjet($projet1);
-        $manager->persist($tache3);
-
-        $tache4 = new Tache();
-        $tache4->setTitre('Déploiement sur l\'App Store')
-            ->setDescription('Vérifier avant que tout fonctionne bien !')
-            ->setStatut($todo)
-            ->setProjet($projet2);
-        $manager->persist($projet2);
-
-        $tache5 = new Tache();
-        $tache5->setTitre('Réalisation des maquettes')
-            ->setDescription('À faire sur Figma')
-            ->setStatut($doing)
+        $tache3->setTitre('Test (Done)')
+            ->setDescription('Une tâche terminée')
+            ->setStatut($done)
             ->setEmploye($employe3)
-            ->setDeadline((new DateTime())->sub(new DateInterval('P18D')))
-            ->setProjet($projet3);
-        $manager->persist($tache5);
-
-        $tache6 = new Tache();
-        $tache6->setTitre('Intégration des maquettes')
-            ->setDescription('Bien faire attention au responsive')
-            ->setStatut($todo)
-            ->setEmploye($employe1)
-            ->setProjet($projet3);
-        $manager->persist($tache6);
-
-        $tache7 = new Tache();
-        $tache7->setTitre('Optimisation du référencement')
-            ->setStatut($todo)
-            ->setDeadline((new DateTime())->sub(new DateInterval('P35D')))
-            ->setProjet($projet3);
-        $manager->persist($tache7);
-
+            ->setProjet($projet3)
+            ->setDeadline(new DateTime('2024-08-16'));
+        $manager->persist($tache3);
 
         $manager->flush();
     }
